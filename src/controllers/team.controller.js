b52af965a36a991a4962c8d7319857c61aeb69f8
AayushParashar28 <ayushparashar6203@gmail.com>
2023-04-11 19:52:57 +0530

    Teams API Completed

--- src/controllers/team.controller.js
+++ src/controllers/team.controller.js
@@ -1,31 +1,86 @@
 const TeamModel = require("../models/team.models");
 
 exports.createTeam = async (req, res) => {
-    try {
-      const team = {
-        Name: req.body.Name,
-        location: req.body.location,
-        owner: req.body.owner,
-        numberofwins: req.body.numberofwins,
-        foundingyear: req.body.foundingyear,
-        dresscolor: req.body.dresscolor,
-        sponser: req.body.sponser,
-        captain: req.body.captain,
-        headcoach: req.body.headcoach,
-        battingcoach: req.body.battingcoach,
-        bowlingcoach: req.body.bowlingcoach
-      };
-  
-      const result = await TeamModel.create(team);
-  
-      return res.status(201).json({
-        msg: result,
-      });
-  
-    } catch (error) {
-      console.log(error);
-      return res.status(500).json({
-        msg: "Internal server error",
-      });
+  try {
+    const team = {
+      Name: req.body.Name,
+      location: req.body.location,
+      owner: req.body.owner,
+      numberofwins: req.body.numberofwins,
+      foundingyear: req.body.foundingyear,
+      dresscolor: req.body.dresscolor,
+      sponser: req.body.sponser,
+      captain: req.body.captain,
+      headcoach: req.body.headcoach,
+      battingcoach: req.body.battingcoach,
+      bowlingcoach: req.body.bowlingcoach
+    };
+
+    const result = await TeamModel.create(team);
+
+    return res.status(201).json({
+      msg: result,
+    });
+
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      msg: "Internal server error",
+    });
+  }
+};
+
+exports.updateTeam = async (req, res) => {
+  try {
+    const team = {
+      Name: req.body.Name
     }
-  };
+    const checkTeam = await TeamModel.findOne({ Name: team.Name })
+
+    if (checkTeam) {
+      checkTeam.location = req.body.location,
+        checkTeam.owner = req.body.owner,
+        checkTeam.numberofwins = req.body.numberofwins,
+        checkTeam.dresscolor = req.body.dresscolor,
+        checkTeam.sponser = req.body.sponser,
+        checkTeam.captain = req.body.captain,
+        checkTeam.headcoach = req.body.headcoach,
+        checkTeam.battingcoach = req.body.battingcoach,
+        checkTeam.bowlingcoach = req.body.bowlingcoach
+
+      const result = await checkTeam.save();
+      return res.status(200).json({
+        "result": result
+      })
+    }
+    else {
+      return res.status(200).json({
+        "msg": "User Not Found"
+      })
+    }
+
+
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      msg: "Internal server error",
+    });
+  }
+}
+
+exports.getAllTeam = async (req, res) => {
+  try {
+    const result = await TeamModel.find()
+
+    return res.status(201).json({
+      sucess: true,
+      response: result,
+    });
+
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      msg: "Internal server error",
+    });
+  }
+}
--- src/routes/team.routes.js
+++ src/routes/team.routes.js
@@ -5,4 +5,8 @@ const TeamRouter = express.Router();
 
 TeamRouter.post("/team", TeamController.createTeam)
 
+TeamRouter.patch("/update", TeamController.updateTeam)
+
+TeamRouter.get("/getAllTeam", TeamController.getAllTeam)
+
 module.exports = TeamRouter
